53ba4dd43abdbc62b88444bcb7f10c640867c274
tinolazreg <73827441+tinolazreg@users.noreply.github.com>
2022-01-07 16:04:47 +0100

    fix org.zalando.problem module being dependent on self (#351)

--- problem/src/main/java/module-info.java
+++ problem/src/main/java/module-info.java
@@ -1,6 +1,5 @@
 module org.zalando.problem {
     requires static org.apiguardian.api;
     requires transitive com.google.gson;
-    requires transitive org.zalando.problem;
     exports org.zalando.problem;
 }
